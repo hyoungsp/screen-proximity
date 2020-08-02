f7cc62b7c5e81c83639153bd33c36e74a7bdb144
hyoungsun <hyoungsun@uchicago.edu>
2020-08-01 21:14:34 -0400

    App to functional comp

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import Router from "./Router";
 import GlobalStyles from "./GlobalStyled";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Router />
-        <GlobalStyles />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
